dd3423bd83415a61761f84af7495101ed5bc8fb3
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2022-03-26 18:50:05 +0100

    updates on front

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,30 +1,89 @@
-<script>
-	export let name;
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	class Task {
+		id: number;
+		title: string;
+		done: boolean;
+
+		deserialize(input: object) {
+			Object.assign(this, input);
+			return this;
+		}
+	}
+
+	let tasks = [];
+	let newTitle: string;
+
+	async function getTasks() {
+		const res = await fetch(`http://localhost:10000/tasks`, {
+			mode: "cors",
+		});
+		tasks = await res.json();
+	}
+
+	async function adTask() {
+		let newTask = new Task();
+		newTask.title = newTitle;
+		newTask.done = false;
+		const res = await fetch(`http://localhost:10000/tasks/create`, {
+			method: "POST",
+			mode: "cors",
+			body: JSON.stringify(newTask),
+		});
+		newTask = new Task().deserialize(await res.json());
+		tasks = [...tasks, newTask];
+		newTitle = "";
+	}
+
+	async function updateTask(task: Task) {
+		const res = await fetch(`http://localhost:10000/tasks/update`, {
+			method: "PUT",
+			mode: "cors",
+			body: JSON.stringify(task),
+		});
+		task = new Task().deserialize(await res.json());
+		const index = tasks.findIndex((element, index) => {
+			if (element.id === task.id) {
+				return true;
+			}
+		});
+		tasks[index] = task;
+		tasks = tasks;
+	}
+
+	async function deleteTask(id: number) {
+		const res = fetch(`http://localhost:10000/tasks/${id}`, {
+			method: "DELETE",
+			mode: "cors",
+		});
+		res.then(() => {
+			tasks = tasks.filter((task) => task.id !== id);
+			tasks = tasks;
+		});
+	}
+
+	function tickTask(task: Task) {
+		task.done = !task.done;
+		updateTask(task);
+	}
+
+	onMount(getTasks);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<ul>
+		{#each tasks as task}
+			<li>
+				{task.title}
+				<input type="checkbox" checked={task.done} on:click={tickTask(task)} />
+				<button on:click={deleteTask(task.id)}>Delete</button>
+			</li>
+		{/each}
+	</ul>
+	<input bind:value={newTitle} />
+	<button on:click={adTask}>Ajouter</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
